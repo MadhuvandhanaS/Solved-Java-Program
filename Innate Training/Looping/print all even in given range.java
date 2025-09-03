5e497e2a7acadf8bf8aa50bdc52f739d2cbb9fb1
MadhuvandhanaS <145217577+MadhuvandhanaS@users.noreply.github.com>
2025-09-03 22:18:25 +0530

    Create print all even in given range.java

---
+++ Innate Training/Looping/print all even in given range.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        for(int i=n; i<=m;i++){
+            if(i%2==0){
+                System.out.print(i+" ");
+            }
+}
+        
+    }
+}
