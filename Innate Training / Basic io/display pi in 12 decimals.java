fbf100f938e38d894e77b36d06e1a2e82a2eab81
MadhuvandhanaS <145217577+MadhuvandhanaS@users.noreply.github.com>
2025-09-03 21:39:59 +0530

    Create display pi in 12 decimals.java

---
+++ Innate Training / Basic io/display pi in 12 decimals.java
@@ -0,0 +1,8 @@
+import java.text.DecimalFormat;
+
+public class PiPrecision {
+    public static void main(String[] args) {
+        double pi = 22.0/7.0;
+        System.out.printf("%.12f",pi);
+    }
+}
