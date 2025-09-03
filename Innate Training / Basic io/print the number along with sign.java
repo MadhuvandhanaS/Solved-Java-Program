d3a3077f5bd1a0a9c47248ae3b2ba85b89046e96
MadhuvandhanaS <145217577+MadhuvandhanaS@users.noreply.github.com>
2025-09-03 21:36:58 +0530

    Create print the number along with sign.java

---
+++ Innate Training / Basic io/print the number along with sign.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n>=0)
+            System.out.println("+"+n);
+        else
+            System.out.println(n);
+    }
+}
