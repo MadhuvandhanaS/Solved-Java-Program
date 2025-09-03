2380240bfbe2138be371dc058c515e3590196a33
MadhuvandhanaS <145217577+MadhuvandhanaS@users.noreply.github.com>
2025-09-03 22:06:41 +0530

    Create check numbers or equals.java

---
+++ Innate Training/Conditionals/check numbers or equals.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n1 = in.nextInt();
+        int n2 = in.nextInt();
+        if(n1==n2)
+            System.out.println("EQUAL");
+        else
+            System.out.println("NOT EQUAL");
+    }
+}
