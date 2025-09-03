3aaac5b257fe8f3eb946dd1b1fb058ab7285fc3c
MadhuvandhanaS <145217577+MadhuvandhanaS@users.noreply.github.com>
2025-09-03 21:54:36 +0530

    Create print division values with only 2 precisions.java

---
+++ Innate Training/Operators/print division values with only 2 precisions.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        double d = a*b;
+        System.out.printf("%.2f",d);
+    }
+}
