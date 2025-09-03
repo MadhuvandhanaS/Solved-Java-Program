922e10b9ffa723c51efde8076201a58dde4bfd6d
MadhuvandhanaS <145217577+MadhuvandhanaS@users.noreply.github.com>
2025-09-03 22:14:11 +0530

    Create print corresponding days based on date.java

---
+++ Innate Training/Conditionals/print corresponding days based on date.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n==0)
+            System.out.println("Sunday");
+        else if(n==1)
+            System.out.println("Monday");
+        else if(n==2)
+            System.out.println("Tuesday");
+        else if(n==3)
+            System.out.println("Wednesday");
+        else if(n==4)
+            System.out.println("Thursday");
+        else if(n==5)
+            System.out.println("Friday");
+        else if(n==6)
+            System.out.println("Saturday");
+        else
+            System.out.println("Invalid");
+    }
+}
