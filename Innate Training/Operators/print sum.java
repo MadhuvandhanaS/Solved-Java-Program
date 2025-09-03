84a6e086b71839da4d42c187cd0e7f50fb705248
MadhuvandhanaS <145217577+MadhuvandhanaS@users.noreply.github.com>
2025-09-03 21:47:20 +0530

    Create print sum.java

---
+++ Innate Training/Operators/print sum.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println("Sum of "+ a +" and "+b+" is "+(a+b));
+    }
+}
