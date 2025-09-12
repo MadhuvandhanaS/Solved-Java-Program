b177ff9dd0e3d8ea5f80fac75b2e3ed923176c73
MadhuvandhanaS <145217577+MadhuvandhanaS@users.noreply.github.com>
2025-09-12 13:06:48 +0530

    Create print no of alphabets.java

---
+++ Innate Training/Looping/print no of alphabets.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println("a b c d e f g h i j k l m n o p q r s t u v w x y z");
+        }
+    }
+}
